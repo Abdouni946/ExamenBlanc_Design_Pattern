4b1bcf4c3a9d32f0f7a4905773cb13e34c913866
Zakariae ABDOUNI <abdouni.zakariae@gmail.com>
2024-12-13 11:26:55 +0100

    Aspect Service

---
+++ src/main/java/org/example/agentManager/AgentService.java
@@ -0,0 +1,29 @@
+package org.example.agentManager;
+
+import org.example.aspects.Cachable;
+import org.example.aspects.Log;
+import org.example.aspects.SecuredBy;
+
+public class AgentService {
+
+    @Log
+    public void performAction() {
+        System.out.println("Performing action...");
+        try {
+            Thread.sleep(500); // Simule un délai
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Cachable
+    public String getMaxTransaction() {
+        System.out.println("Calculating max transaction...");
+        return "Transaction with the highest amount";
+    }
+
+    @SecuredBy(roles = {"ROLE_ADMIN"})
+    public void secureMethod() {
+        System.out.println("Executing a secure method...");
+    }
+}
